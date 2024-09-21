588a9ca36ba0f6a0a2d984633f61e643b5e0ec2f
LEANDRO CAPONETTO <caponettopeppers@gmail.com>
2024-09-21 13:10:18 -0300

    fix-service

--- src/main/java/com/leandro/controller/UserController.java
+++ src/main/java/com/leandro/controller/UserController.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -22,6 +24,20 @@ public class UserController {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
+        try {
+            UserDTO user = userService.getUserById(id);
+            return ResponseEntity.ok(user); // Devuelve el usuario con estado 200 OK si lo encuentra
+        } catch (HttpClientErrorException.NotFound e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(null); // Devuelve un 404 si el usuario no se encuentra
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null); // Devuelve un 500 si ocurre algún error
+        }
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String save(@RequestBody UserDTO user){
@@ -29,6 +45,24 @@ public class UserController {
         return "Usuario creado ok";
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updataUser(@PathVariable Long id, @RequestBody UserDTO user){
+        userService.updatUser(id, user);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        boolean deleted = userService.deleteUser(id);
+
+        if (deleted) {
+           return ResponseEntity.ok("El id " + id + " se borró correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo borrar el id " + id + ", ya que no existe.");
+        }
+
+    }
 
 
 }
--- src/main/java/com/leandro/service/UserService.java
+++ src/main/java/com/leandro/service/UserService.java
@@ -2,8 +2,11 @@ package com.leandro.service;
 
 import com.leandro.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     @Value("${spring.external.service.base-url}")
@@ -22,8 +26,37 @@ public class UserService {
         UserDTO[] response = restTemplate.getForObject(basePath+"/users", UserDTO[].class);
         return Arrays.asList(response);
     }
+    public UserDTO getUserById(Long id) {
+        String url = String.format("%s/users/%d", basePath, id);
+
+        try {
+            return restTemplate.getForObject(url, UserDTO.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            // Maneja cuando el usuario no se encuentra (404)
+            log.error("Usuario con id {} no encontrado.", id);
+            throw new RuntimeException("Usuario no encontrado");
+        } catch (RestClientException e) {
+            // Maneja otros errores relacionados con la comunicación HTTP
+            log.error("Error al comunicarse con el servicio: {}", e.getMessage());
+            throw new RuntimeException("Error al obtener el usuario");
+        }
+    }
+
     public String saveUser(UserDTO user){
          restTemplate.postForObject(basePath+"/users",user, UserDTO.class);
         return "Usuer save ok {}" ;
+    }
+
+    public void updatUser(Long id, UserDTO user){
+        restTemplate.put(basePath+"/users/"+id, user);
+    }
+
+    public  boolean deleteUser(Long id){
+        try {
+            restTemplate.delete(basePath + "/users/" + id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
